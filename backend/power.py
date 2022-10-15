624b3cb2336bd660ddfa507412cc2321937479f7
Samuel Gerstein <samuelgerstein@wirelessprv-10-195-78-5.near.illinois.edu>
2022-10-15 14:43:05 -0500

    adding new power.py file

---
+++ backend/power.py
@@ -0,0 +1,21 @@
+import location
+class power:
+    def __init__(self, location : location.Location):
+        self.location = location
+    def get_solar_power(self) -> float:
+        """
+        """
+        return 0.0
+    def get_wind_power(self) -> float:
+        """
+        """
+        return 0.0
+    def get_nuclear_power(self) -> float:
+        """
+        """
+        return 0.0
+    def get_geothermal_power(self) -> float:
+        """
+        """
+        return 0.0
+    
