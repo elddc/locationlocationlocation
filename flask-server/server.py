035b239a31c18455963b516500a9000fbbfb7de4
sbaek21 <jasmine4081@gmail.com>
2022-10-15 18:44:05 -0500

    Added flask server

---
+++ flask-server/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+api = Flask(__name__)
+@api.route('/locationlocationlocation')
+def locationlocationlocation():
+    response_body = {
+        "Energy" : "Wind"
+    }
+
+    return response_body
